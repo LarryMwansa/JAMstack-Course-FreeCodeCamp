261dea54ec5a6359ce727515b4c9353158446b89
Larry Mwansa <larrypmwansa@gmail.com>
2023-07-07 17:03:58 +0200

    unhide key

--- src/_data/news.js
+++ src/_data/news.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 module.exports = async function() {
     try {
-      const response = await axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}&pageSize=5');
+      const response = await axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=82e92f91f00d4fc28a2acf43d211dc12&pageSize=5');
       return response.data;
     } catch (error) {
       console.error(error);
